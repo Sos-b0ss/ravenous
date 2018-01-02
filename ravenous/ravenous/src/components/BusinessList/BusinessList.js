a7c5fd58eb0d4ef00f87bf1e2989c826f9f306ae
Janu <root@Potat.OS>
2018-01-01 21:54:48 -0700

    First initializating commit using git

---
+++ ravenous/ravenous/src/components/BusinessList/BusinessList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './BusinessList.css';
+
+import Business from '../Business/Business';
+
+class BusinessList extends React.Component
+{ render(
+  <div className="BusinessList">
+        {/* Add Business Component here */}
+        {/* Add Business Component here */}
+        {/* Add Business Component here */}
+        {/* Add Business Component here */}
+        {/* Add Business Component here */}
+        {/* Add Business Component here */}
+      </div>
+    );
+
+}
